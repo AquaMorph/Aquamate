087442688987734c740fdf2a93b27d86b53033e0
Christian Colglazier <ccolglazier@lenovo.com>
2018-09-24 16:15:52 -0400

    Added template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'name': 'Christian Colglazier'}
+    return render_template('index.html', title='Home', user=user)
